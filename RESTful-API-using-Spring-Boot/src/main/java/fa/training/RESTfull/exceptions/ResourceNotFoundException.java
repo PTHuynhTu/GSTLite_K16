c93c09a8fa05c8bb5acb6771e85a4d65256a8c82
HuynhTu <37186988+PTHuynhTu@users.noreply.github.com>
2020-06-26 18:49:33 +0700

    commit GSTLite project spring restful api

---
+++ RESTful-API-using-Spring-Boot/src/main/java/fa/training/RESTfull/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package fa.training.RESTfull.exceptions;
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String message) {
+         super(message);
+    }
+}
---
+++ RESTful-API-using-Spring-Boot/src/test/java/fa/training/RESTfull/API/using/Spring/Boot/RestfullApiUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fa.training.RESTfull.API.using.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullApiUsingSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
