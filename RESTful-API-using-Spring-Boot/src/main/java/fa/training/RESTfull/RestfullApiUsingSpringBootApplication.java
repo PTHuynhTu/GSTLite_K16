fe7bece040424d62ccabb5a7f7d12450348ee250
Nguyễn Văn Hùng <35483266+HungNguyenGeorge@users.noreply.github.com>
2020-07-07 08:23:39 +0700

    Sửa lỗi cho các dev dùng eclipse

--- RESTful-API-using-Spring-Boot/src/main/java/fa/training/RESTfull/RestfullApiUsingSpringBootApplication.java
+++ RESTful-API-using-Spring-Boot/src/main/java/fa/training/RESTfull/RestfullApiUsingSpringBootApplication.java
@@ -3,25 +3,25 @@ package fa.training.RESTfull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 //@EnableJpaAuditing
 //@EnableJpaRepositories("fa.training.RESTfull")
-@EnableSwagger2
+//@EnableSwagger2
 public class RestfullApiUsingSpringBootApplication {
-	@Bean
-	public Docket API() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("fa.training.RESTfull"))
-				.paths(PathSelectors.regex("/.*"))
-				.build();
-	}
+//	@Bean
+//	public Docket API() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.select()
+//				.apis(RequestHandlerSelectors.basePackage("fa.training.RESTfull"))
+//				.paths(PathSelectors.regex("/.*"))
+//				.build();
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfullApiUsingSpringBootApplication.class, args);
